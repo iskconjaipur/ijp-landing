e8c835a49dce96607939b85b575ad10214d0303f
vipinsharma0999 <64528003+iskconjaipur@users.noreply.github.com>
2024-10-26 18:24:06 +0530

    updated shipping policy

---
+++ src/app/(policies)/shipping-policy/page.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div className="p-6 max-w-screen-lg pt-24">
+      <h1 className="text-3xl font-bold mb-4">Shipping Policy</h1>
+      <p className="text-lg text-justify">
+        Our products/services are digital or provided online, and therefore, no physical items will be shipped.
+        Upon successful purchase or subscription, you will receive immediate access to the digital products or services.
+        If you have any issues accessing your purchased content, please contact our support team.
+      </p>
+    </div>
+  );
+};
+
+export default page;
+
+
