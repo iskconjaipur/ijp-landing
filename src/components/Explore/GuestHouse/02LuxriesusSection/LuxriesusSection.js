fab217fcc8ebf76cf0040b7a0b10411e78c49d5a
saraswate <64528003+iskconjaipur@users.noreply.github.com>
2025-07-16 13:04:22 +0530

    updates for janmasthami

--- src/components/Explore/GuestHouse/02LuxriesusSection/LuxriesusSection.js
+++ src/components/Explore/GuestHouse/02LuxriesusSection/LuxriesusSection.js
@@ -1,16 +1,16 @@
 "use client";
 import React from "react";
-import {Playfair_Display } from "next/font/google";
+import { Playfair_Display } from "next/font/google";
 
 const playfair = Playfair_Display({
   subsets: ["latin"],
 });
 
-export default function LuxriesusSection()  {
+export default function LuxriesusSection() {
   return (
     <>
       {/* <luxriesus section start> */}
-      <div className=" flex flex-col mx-[30px] sm:mx-[92px] gap-9 my-[70px] py-[30px] lg:flex-row">
+      <div className=" flex flex-col mx-[30px] sm:mx-[92px] gap-9 my-[70px] py-[30px] lg:flex-row justify-between items-center">
         <div className=" w-full lg:w-1/2 flex  flex-col gap-4">
           <div className=" p-[10px]">
             <p className="text-[#D9A55B] text-[22px] tracking-widest">
@@ -24,13 +24,19 @@ export default function LuxriesusSection()  {
           </div>
           <div className="">
             <p className="text-[#6C6D72] text-[16px] text-justify p-[18px]">
-            The guest house is inside the temple premises which is filled with a spiritual atmosphere. The rooms are neat and clean, the staff is very friendly and you can attend various temple programs during your stay.
+              The guest house is inside the temple premises which is filled with
+              a spiritual atmosphere. The rooms are neat and clean, the staff is
+              very friendly and you can attend various temple programs during
+              your stay.
             </p>
           </div>
           <div className="flex  gap-5 flex-col md:flex-row">
             <div className=" flex flex-col w-full md:w-1/2 ">
               <div className=" w-[100px] h-[90px] p-[10px]">
-                <img className="w-[60px] h-[70px] " src="/Explore/GuestHouse/class.png"></img>
+                <img
+                  className="w-[60px] h-[70px] "
+                  src="/Explore/GuestHouse/class.png"
+                ></img>
               </div>
               <div className="">
                 <h2 className="text-[22px] text-[#091826] font-medium p-[10px]">
@@ -39,13 +45,17 @@ export default function LuxriesusSection()  {
               </div>
               <div className="">
                 <p className="text-[#6C6D72] text-[16px] text-justify p-[15px]">
-                 our rooms have both nice infrastructure and comfortable environment for our guests.
+                  our rooms have both nice infrastructure and comfortable
+                  environment for our guests.
                 </p>
               </div>
             </div>
-            <div className=" w-full md:w-1/2 ">
+            <div className=" w-full md:w-3/5 ">
               <div className=" w-[100px] h-[90px] p-[10px] ">
-                <img className="w-[60px] h-[70px]" src="/Explore/GuestHouse/Iife2.png"></img>
+                <img
+                  className="w-[60px] h-[70px]"
+                  src="/Explore/GuestHouse/Iife2.png"
+                ></img>
               </div>
               <div className="">
                 <h2 className="text-[22px] text-[#091826] font-medium p-[10px]">
@@ -54,7 +64,8 @@ export default function LuxriesusSection()  {
               </div>
               <div className="">
                 <p className="text-[#6C6D72] text-[16px] text-justify p-[15px]">
-                  the staff and the management is very helpful and friendly. The room services is also up to the mark.
+                  the staff and the management is very helpful and friendly. The
+                  room services is also up to the mark.
                 </p>
               </div>
             </div>
@@ -62,10 +73,10 @@ export default function LuxriesusSection()  {
         </div>
 
         {/* for image */}
-        <div className=" w-full lg:w-1/2">
+        <div className="w-full lg:w-2/5 max-h-[80vh] overflow-hidden">
           <img
-            src="/Explore/GuestHouse/img.jpeg"
-            className="w-full h-full object-cover"
+            src="/Explore/GuestHouse/ijp-guesth-janmasthami.jpeg"
+            className="w-full h-auto max-h-[80vh] object-contain"
             alt="Image"
           />
         </div>
@@ -73,4 +84,4 @@ export default function LuxriesusSection()  {
       {/* <luxriesus section ent> */}
     </>
   );
-};
+}
